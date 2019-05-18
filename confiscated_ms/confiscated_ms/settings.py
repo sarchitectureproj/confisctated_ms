5d7924484f9e9178527d868bfba1896033c66a1b
Edwin Jimenez <ejjimenezc@unal.edu.co>
2019-05-18 18:37:46 -0500

    Conexion a MYSQL

--- confiscated_ms/confiscated_ms/settings.py
+++ confiscated_ms/confiscated_ms/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'confiscated_ms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'confiscated_items_db',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
