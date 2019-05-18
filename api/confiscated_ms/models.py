b6aeac295837d764b0d84b43cd453cb0158e0f6f
Edwin Jimenez <ejjimenezc@unal.edu.co>
2019-05-18 10:11:03 -0500

    Serializers added

--- api/confiscated_ms/models.py
+++ api/confiscated_ms/models.py
@@ -14,6 +14,8 @@ class Item(models.Model):
         null=True,
     )
     passenger = models.IntegerField(blank=False)
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
@@ -25,10 +27,14 @@ class Category(models.Model):
         on_delete=models.SET_NULL,
         null=True,
     )
+    def __str__(self):
+        return self.name
 
 class Delivery(models.Model):
     id = models.AutoField(primary_key=True)
     open_time = models.TimeField()
     close_time = models.TimeField()
     description = models.CharField(max_length=255,blank=True,unique=True,null=True)
+    def __str__(self):
+        return self.name
     
